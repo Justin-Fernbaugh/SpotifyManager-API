b0d6d108cc33c1ee5872a5f79b14769e107944df
Justin <fernbaughj@gmail.com>
2022-09-18 13:31:45 -0700

    Implemented /playlist/all route

--- routes/playlistRoutes.js
+++ routes/playlistRoutes.js
@@ -1,11 +1,41 @@
+const { response } = require("express");
 const express = require("express");
-const SpotifyWebApi = require('spotify-web-api-node');
 
 const Router = express.Router();
 
-module.exports = (SpotifyWebApi) => {
+module.exports = (SpotifyAPI) => {
 	Router.get('/health', (req, res) => {
 		res.send('HEALTHY');
 	})
+
+	Router.get('/playlist/all', async (req, res) => {
+		const {accessToken, refreshToken} = req.query;
+		SpotifyAPI.setAccessToken(accessToken);
+		SpotifyAPI.setRefreshToken(refreshToken);
+		
+		try {
+			console.log('before');
+			console.log('after');
+			const user = await SpotifyAPI.getMe();
+			console.log('User request', user);
+
+			SpotifyAPI.getUserPlaylists(user.body['display_name'])
+			.then(function(data) {
+				// console.log('Retrieved playlists', data.body.items);
+				res.send(data.body.items);
+			},function(err) {
+				console.log('getUserPlaylist has failed', err);
+				res.status(500).send('Internal server error: /playlist/all');
+			});
+		} catch(error) {
+			console.log(`Error: ${error}`);
+			const data = await SpotifyAPI.refreshAccessToken();
+			SpotifyAPI.setAccessToken(data.body['access_token']);
+			res.status(401).send('Token has expired; Refresh token and try again.');
+		}
+	})
+
+
+
 	return Router;
 }
